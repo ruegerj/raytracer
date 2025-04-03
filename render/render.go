c80170b2dec0d2c91794bd039b1ef8dae4346885
ruegerj <jan.rueger@gmx.ch>
2025-04-03 23:12:12 +0200

    add parallel computation as a first draft

--- render/render.go
+++ render/render.go
@@ -2,7 +2,8 @@ package render
 
 import (
 	"image"
-	"image/color"
+	"log"
+	"sync"
 
 	"github.com/ruegerj/raytracing/primitive"
 	"github.com/ruegerj/raytracing/scene"
@@ -15,20 +16,35 @@ func Do(world *scene.World, img *image.RGBA) {
 	height := img.Bounds().Dy()
 	acceptAnyHit := func(_ *scene.Hit, _ scene.Hitable) bool { return true }
 
+	imageBuffer := createImageBuffer(width, height)
+
+	var wg sync.WaitGroup
+	wg.Add(height)
+
 	for y := range height {
-		for x := range width {
-			r := world.Camera().RayFrom(x, y)
-			hit, hasHit := world.Hits(r, acceptAnyHit)
+		go func() {
+			defer wg.Done()
 
-			if !hasHit {
-				img.Set(x, y, color.Black)
-				continue
+			for x := range width {
+				r := world.Camera().RayFrom(x, y)
+				hit, hasHit := world.Hits(r, acceptAnyHit)
+
+				if !hasHit {
+					imageBuffer[y][x] = primitive.ScalarColor{R: 0, G: 0, B: 0}
+					continue
+				}
+
+				c := calcColor(hit, world, true)
+				imageBuffer[y][x] = c
 			}
-
-			c := calcColor(hit, world, true)
-			img.Set(x, y, c.ToRGBA())
-		}
+		}()
 	}
+
+	wg.Wait()
+
+	exportBufferToImage(imageBuffer, img)
+
+	log.Println("done")
 }
 
 func calcColor(hit *scene.Hit, world *scene.World, ambient bool) primitive.ScalarColor {
@@ -74,4 +90,21 @@ func avgLightFactor(lightFactors []float32) float32 {
 	}
 
 	return sum / float32(len(lightFactors))
+}
+
+func createImageBuffer(width, height int) [][]primitive.ScalarColor {
+	imageBuffer := make([][]primitive.ScalarColor, height)
+	for y := range imageBuffer {
+		imageBuffer[y] = make([]primitive.ScalarColor, width)
+	}
+
+	return imageBuffer
+}
+
+func exportBufferToImage(imageBuffer [][]primitive.ScalarColor, img *image.RGBA) {
+	for y := range imageBuffer {
+		for x := range imageBuffer[y] {
+			img.Set(x, y, imageBuffer[y][x].ToRGBA())
+		}
+	}
 }
